325677f77a9def9af480ee83b6f346560a65e719
Michael Samsonenko <samsonenkoit@hotmail.com>
2026-07-16 17:49:24 +0600

    +

--- Database/Models/SubtaskDb.cs
+++ Database/Models/SubtaskDb.cs
@@ -8,6 +8,12 @@ public class SubtaskDb
 
     public int TaskId { get; set; }
 
+    public string? Description { get; set; }
+
+    public DateTime DueDateUtc { get; set; }
+
+    public int ProgressPercent { get; set; }
+
     public TaskDb Task { get; set; } = null!;
 
     public DateTime? DeletedAtUtc { get; set; }
